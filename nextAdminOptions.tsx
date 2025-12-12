7d5b3c397cd67aa8ab10548c21b459ec677bcd41
luisguareschi <luisguareschi@hotmail.com>
2025-12-12 18:07:03 +0100

    Updated admin look & feel

--- nextAdminOptions.tsx
+++ nextAdminOptions.tsx
@@ -1,13 +1,13 @@
 import type { NextAdminOptions } from "@premieroctet/next-admin";
-import { Component } from "lucide-react";
+import { ShieldCheck } from "lucide-react";
 
 // Check https://heroicons.com/ for icon names
 
 const options: NextAdminOptions = {
   title: (
-    <div className="flex items-center justify-center gap-4 px-4">
-      <Component className="min-w-5 min-h-5" />
-      <p>BaseApp Admin</p>
+    <div className="flex items-center justify-center gap-4 px-4 text-slate-600">
+      <ShieldCheck className="min-w-5 min-h-5" />
+      <p className="font-bold">BaseApp Admin</p>
     </div>
   ),
   forceColorScheme: "light",
