71a8fd41be43d08d52cfe2fd60f1e319862fe6a8
luisguareschi <luisguareschi@hotmail.com>
2025-12-19 16:08:24 +0100

    Updated BarLoader component to use a new color scheme and simplified styling.

--- components/common/bar-loader.tsx
+++ components/common/bar-loader.tsx
@@ -1,17 +1,9 @@
-import { useTheme } from "next-themes";
 import BaseBarLoader from "react-spinners/BarLoader";
 import { LoaderHeightWidthProps } from "react-spinners/helpers/props";
 
 interface BarLoaderProps extends LoaderHeightWidthProps {}
 
 export const BarLoader = (props: BarLoaderProps) => {
-  const theme = useTheme();
-  const color = theme.theme === "dark" ? "#fafafa" : "#18181b";
-  return (
-    <BaseBarLoader
-      color={color}
-      className="bg-ios-gray-900 dark:bg-ios-gray-50"
-      {...props}
-    />
-  );
+  const color = "oklch(62.3% 0.214 259.815)";
+  return <BaseBarLoader color={color} className="bg-blue-500" {...props} />;
 };
