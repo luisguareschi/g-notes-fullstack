f663c2f4d470ab7e769724ff2d21bc9826fd8abe
luisguareschi <luisguareschi@hotmail.com>
2026-01-12 11:23:41 +0100

    Extracted accounts list

--- app/(logged)/account-list/page.tsx
+++ app/(logged)/account-list/page.tsx
@@ -13,6 +13,7 @@ import { IOSFormCard } from "@/components/common/ios-form/ios-form-card";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { ChevronRight } from "lucide-react";
 import { CreateAccountButton } from "@/components/common/create-account-button";
+import { AccountsCredentialsList } from "@/components/common/accounts-credentials-list";
 
 const AccountListPageQueryParams = z.object({
   type: z
@@ -79,41 +80,11 @@ const AccountListPage = () => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      {!!accountCredentials?.length && !isLoadingAccountCredentials && (
-        <IOSFormCard fullWidthSeparator className="mb-30">
-          {accountCredentials?.map((account) => {
-            return (
-              <div
-                key={account.id}
-                className="flex items-center gap-2 cursor-pointer"
-                onClick={() => handleAccountClick(account.id)}
-              >
-                <Avatar className="size-9">
-                  <AvatarFallback className="uppercase">
-                    {account.name.charAt(0)}
-                    {account.name.charAt(1)}
-                  </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col">
-                  <p className="font-medium">{account.name}</p>
-                  <p className="text-sm text-ios-gray-500">
-                    {account.email || account.username}
-                  </p>
-                </div>
-                <ChevronRight className="w-5 h-5 text-ios-gray-500 ml-auto" />
-              </div>
-            );
-          })}
-        </IOSFormCard>
-      )}
-      {!isLoadingAccountCredentials && accountCredentials?.length === 0 && (
-        <div className="flex flex-col items-center justify-center mt-10">
-          <p className="text-sm text-ios-gray-500">No accounts found</p>
-        </div>
-      )}
-      {isLoadingAccountCredentials && (
-        <div className="w-full h-30 bg-ios-gray-200 dark:bg-ios-gray-900 rounded-lg animate-pulse" />
-      )}
+      <AccountsCredentialsList
+        accountCredentials={accountCredentials}
+        isLoading={isLoadingAccountCredentials}
+        handleAccountClick={handleAccountClick}
+      />
       <CreateAccountButton removeNavbarPadding />
     </div>
   );
---
+++ components/common/accounts-credentials-list.tsx
@@ -0,0 +1,56 @@
+import { GetAccountCredentialsResponseItem } from "@/orval/generated/openAPI.schemas";
+import { IOSFormCard } from "./ios-form/ios-form-card";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { ChevronRight } from "lucide-react";
+
+interface AccountsCredentialsListProps {
+  accountCredentials?: GetAccountCredentialsResponseItem[];
+  isLoading?: boolean;
+  handleAccountClick?: (accountId: string) => void;
+}
+
+export const AccountsCredentialsList = ({
+  accountCredentials,
+  isLoading,
+  handleAccountClick,
+}: AccountsCredentialsListProps) => {
+  return (
+    <div>
+      {!!accountCredentials?.length && !isLoading && (
+        <IOSFormCard fullWidthSeparator className="mb-30">
+          {accountCredentials?.map((account) => {
+            return (
+              <div
+                key={account.id}
+                className="flex items-center gap-2 cursor-pointer"
+                onClick={() => handleAccountClick?.(account.id)}
+              >
+                <Avatar className="size-9">
+                  <AvatarFallback className="uppercase">
+                    {account.name.charAt(0)}
+                    {account.name.charAt(1)}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <p className="font-medium">{account.name}</p>
+                  <p className="text-sm text-ios-gray-500">
+                    {account.email || account.username}
+                  </p>
+                </div>
+                <ChevronRight className="w-5 h-5 text-ios-gray-500 ml-auto" />
+              </div>
+            );
+          })}
+        </IOSFormCard>
+      )}
+      {!isLoading && accountCredentials?.length === 0 && (
+        <div className="flex flex-col items-center justify-center mt-10">
+          <p className="text-sm text-ios-gray-500">No accounts found</p>
+        </div>
+      )}
+      {isLoading && (
+        <div className="w-full h-30 bg-ios-gray-200 dark:bg-ios-gray-900 rounded-lg animate-pulse" />
+      )}
+    </div>
+  );
+};
